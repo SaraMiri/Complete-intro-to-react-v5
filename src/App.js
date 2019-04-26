db9ab9e430596305d50a6de0827b769b5d2b8fa2
Sara Miragaya Rico <saramiragaya@gmail.com>
2019-04-26 16:01:10 +0200

    Add ESlint configuration, unfrak dependency and rewrite App, Pet components on JSX

--- src/App.js
+++ src/App.js
@@ -3,23 +3,13 @@ import ReactDOM from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "brand" }, "Adopt me!"),
-    React.createElement(Pet, {
-      name: "Perdita",
-      animal: "Dog",
-      breed: "Mix"
-    }),
-    React.createElement(Pet, {
-      name: "Lume",
-      animal: "Dog",
-      breed: "Labrador Retriever"
-    }),
-    React.createElement(Pet, {
-      name: "Kai",
-      animal: "Rabbit",
-      breed: "Belier"
-    })
-  ]);
+  return (
+    <div>
+      <h1>Adopt me!</h1>
+      <Pet name="Perdita" animal="Dog" breed="Mix" />
+      <Pet name="Lume" animal="Dog" breed="Labrador Retriever" />
+      <Pet name="Kai" animal="Rabbit" breed="Belier" />
+    </div>
+  );
 };
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
