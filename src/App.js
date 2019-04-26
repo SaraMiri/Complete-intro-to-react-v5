a855dab49df642a707c9860f4b58261512611e99
Sara Miragaya Rico <saramiragaya@gmail.com>
2019-04-26 14:34:07 +0200

    Create Pet component and add React and ReactDOM to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-/* global React ReactDOM */
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
 
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ]);
-};
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", { id: "brand" }, "Adopt me!"),
