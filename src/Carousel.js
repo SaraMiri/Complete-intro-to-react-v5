fd18293191c60f0a247ed82cdb31032822e6bc62
Sara Miragaya Rico <saramiragaya@gmail.com>
2019-05-30 22:30:43 +0200

    WIP: Add Carousel

---
+++ src/Carousel.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+class Carousel extends React.Component {
+  state = {
+    photos: [],
+    active: 0
+  };
+  static getDerivedStateFromProps({ media }) {
+    let photos = [];
+    if (media && media.photos && media.photos.photo) {
+      photos = media.photos.photo.filter(photo => photo["@size"] === "pn");
+    }
+    return { photos };
+  }
+  render() {
+    const { photos, active } = this.state;
+    return (
+      <div className="carousel">
+        <img src={photos[active].value} alt="animal" />
+        <div className="carousel-smaller">
+          {photos.map((photo, index) => (
+            <img
+              className={index === active ? "active" : " "}
+              src={photo.value}
+              key={photo.value}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
