552c846430be91db6ab72857b44ecdffd5b12b74
Sara Miragaya Rico <saramiragaya@gmail.com>
2019-07-05 16:58:33 +0200

    Created node.js server

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
-import React, { useState, lazy, Suspense } from "react";
+import React, { useState } from "react";
 import { Router, Link } from "@reach/router";
+import Details from "./Details";
+import SearchParams from "./SearchParams";
 import ThemeContext from "./ThemeContext";
-
-const Details = lazy(() => import("./Details"));
-const SearchParams = lazy(() => import("./SearchParams"));
-
-const FourOhFour = () => <h1>404</h1>;
 
 const App = () => {
   const theme = useState("darkblue");
   return (
-    <React.StrictMode>
-      <ThemeContext.Provider value={theme}>
-        <div>
-          <header>
-            <Link to="/">Adopt me!</Link>
-          </header>
-          <Suspense fallback={<h1>hey stuff is loading...</h1>}>
-            <Router>
-              <SearchParams path="/" />
-              <Details path="/details/:id" />
-              <FourOhFour default />
-            </Router>
-          </Suspense>
-        </div>
-      </ThemeContext.Provider>
-    </React.StrictMode>
+    <ThemeContext.Provider value={theme}>
+      <div>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        ;
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    </ThemeContext.Provider>
   );
 };
 
